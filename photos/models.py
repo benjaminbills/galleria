e06de42a19600d89be6591129398606cbd261ae6
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-14 12:25:53 +0300

    default value bug on migration

--- photos/models.py
+++ photos/models.py
@@ -5,8 +5,8 @@ class Image(models.Model):
   name = models.CharField(max_length=255)
   posted_date = models.DateTimeField(auto_now_add=True)
   image_description = models.CharField(max_length=500, default='DEFAULT VALUE')
-  location = models.ForeignKey('Location', on_delete=models.CASCADE)
-  category = models.ForeignKey('Category', on_delete=models.CASCADE)
+  location = models.ForeignKey('Location', on_delete=models.CASCADE, default=0)
+  category = models.ForeignKey('Category', on_delete=models.CASCADE, default=0)
   
   def __str__(self):
         return self.name
