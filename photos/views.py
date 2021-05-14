849f0c2f40c0e1ba160c6b5febb9d949ed15e29a
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-15 01:10:13 +0300

    upload to cloudinary

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from . models import Image
+from .models import Image
 # Create your views here.
 def index(request):
   images = Image.objects.all()
+  print(images)
   return render(request, 'photos/index.html', {'images':images})
