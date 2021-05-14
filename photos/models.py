91fca9439b8910c1a0843dab1358d84f775d564f
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-14 12:09:55 +0300

    add location and category to Image class

--- photos/models.py
+++ photos/models.py
@@ -5,7 +5,9 @@ class Image(models.Model):
   name = models.CharField(max_length=255)
   posted_date = models.DateTimeField(auto_now_add=True)
   image_description = models.CharField(max_length=500, default='DEFAULT VALUE')
-
+  location = models.ForeignKey('Location', on_delete=models.CASCADE)
+  category = models.ForeignKey('Category', on_delete=models.CASCADE)
+  
   def __str__(self):
         return self.name
 
