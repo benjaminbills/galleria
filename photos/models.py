dc6cb7f6f73d64bf7b7930e7584ebf5dd4058fce
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-14 12:06:26 +0300

    add category class to model

--- photos/models.py
+++ photos/models.py
@@ -5,11 +5,17 @@ class Image(models.Model):
   name = models.CharField(max_length=255)
   posted_date = models.DateTimeField(auto_now_add=True)
   image_description = models.CharField(max_length=500, default='DEFAULT VALUE')
-  
+
   def __str__(self):
         return self.name
 
 class Location(models.Model):
+  name = models.CharField(max_length=255)
+  #magic method
+  def __str__(self): 
+    return self.name
+
+class Category(models.Model):
   name = models.CharField(max_length=255)
   #magic method
   def __str__(self): 
