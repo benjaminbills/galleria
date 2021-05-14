5ad2e66e2e9d68849b0fc5e49af8abfccc34a3bc
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-14 11:59:50 +0300

    add image_description to Image Model

--- photos/models.py
+++ photos/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-  title = models.CharField(max_length=255)
+  name = models.CharField(max_length=255)
   posted_date = models.DateTimeField(auto_now_add=True)
-
+  image_description = models.CharField(max_length=500, default='DEFAULT VALUE')
   def __str__(self):
         return self.name
