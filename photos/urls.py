a3071b899f97e7d5ffd9a586915a7afb6da3038f
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-17 11:32:34 +0300

    fixed route bug

--- photos/urls.py
+++ photos/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from django.urls.conf import include
 from . import views
 
-#movies/
-#movies/1/details
-app_name = 'movies'
 urlpatterns = [
   path('',views.index, name='index'),
+  path('search/', views.search_results, name='search_results'),
+  path('<int:image_id>', views.detail, name='detail')
 ]
+
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Image
 # Create your views here.
 def index(request):
   images = Image.objects.all()
   print(images)
   return render(request, 'photos/index.html', {'images':images})
+
+
+def detail(request, image_id):
+  image = get_object_or_404(Image, pk=image_id)
+  return render(request, 'photos/detail.html', {'image': image})
+
+def search_results(request):
+  if 'search' in request.GET and request.GET['search']:
+      search_term = request.GET.get('search')
+      searched_images = Image.search_by_category(search_term)
+      message = f"{search_term}"
+      return render(request, 'photos/search.html',{"message":message,"images": searched_images})
+
+  else:
+      message = "You haven't searched for any term"
+      return render(request, 'photos/search.html',{"message":message})
